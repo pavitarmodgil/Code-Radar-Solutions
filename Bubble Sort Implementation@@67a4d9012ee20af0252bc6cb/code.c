9838baa1f1193fc05b0e12593b0273bed22a7f18
Pavitar Modgil <pavitarmodgil001@gmail.com>
2025-04-01 10:09:45 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[],int n) {
+void bubbleSort(int arr[], int n) {
     int temp;
 
-    for(int i=0;i<n-1;i++) {
-        int swapped = 0;
-        for(int j=0;j<n-i-1;j++) {
-            if(arr[j] > arr[j+1]) {
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0; // Reset swap flag at the beginning of each iteration
+        
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) { 
+                // Correct swap logic
                 temp = arr[j];
-                arr[j] = arr[j];
-                arr[j+1] = arr[j];
-                swapped+=1;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                
+                swapped = 1; // Mark swap occurred
             }
-            if (!swapped)
-                break; 
-    }
+        }
+
+        // If no swap happened in this pass, array is already sorted
+        if (!swapped)
+            break;
     }
 }
 
 void printArray(int arr[], int n) {
-    for(int i=0;i<n;i++) {
-        printf("%d", arr[i]);
-        printf("\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]); // Print numbers with spaces
     }
+    printf("\n"); // New line at the end
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    int arr[n];
-    for (int i=0;i<n;i++) {
+    int arr[n]; // Variable length array (C99+)
+
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
     bubbleSort(arr, n);
     printArray(arr, n);
+
     return 0;
 }
-
